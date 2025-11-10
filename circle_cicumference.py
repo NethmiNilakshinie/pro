47c1bc148ae2d45c1bf5e2ac7c760fcac17f6eb6
NethmiNilakshinie <nnilakshani3@gmail.com>
2025-11-10 19:39:51 +0530

    Add files via upload

---
+++ circle_cicumference.py
@@ -0,0 +1,37 @@
+import math
+
+#x = 9.1
+
+#print(math.pi)
+#print(math.e)
+#results = math.sqrt(x)   >>> x = 9
+#results = math.ceil(x)   >>> x = 9.1
+#results = math.floor(x)   >>> x = 9.9
+
+#print(results)
+
+
+radius = float(input("Enter radius: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"The  circumference is: {round(circumference,2)}cm")
+
+
+
+print(">>>>>>>>>>>Cicle Area>>>>>>>>>>>")
+
+
+
+radius2 = float(input("Enter radius: "))
+
+Area = math.pi *pow(radius2,2)
+
+print(f"The  circumference is: {round(Area,2)}cm²")
+
+
+
+
+
+
+
